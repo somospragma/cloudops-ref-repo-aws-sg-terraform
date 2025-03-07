90e8cc9fd800f9b7b1f9421d5896eeaab02d3597
farciniegas52 <felipe.arciniegas@pragma.com.co>
2025-03-07 14:45:26 -0500

    Update SG Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
 output "sg_info" {
-  value = {
-    for k, sg in aws_security_group.sg : k => {
-      "sg_id" : sg.id,
-      "sg_name" : sg.name
-    }
-  }
+  value = { for k, sg in aws_security_group.sg : k => { id = sg.id, name = sg.name } }
 }
--- variables.tf
+++ variables.tf
@@ -22,52 +22,32 @@ variable "project" {
 ###########################################
 
 variable "sg_config" {
-  type = list(object({
-    description = string
-    vpc_id      = string
-    service     = string
-    application = string
+  type = map(object({
+    description   = string
+    vpc_id        = string
+    service       = string
+    application   = string
     functionality = optional(string)
+
     ingress = list(object({
-      from_port       = string
-      to_port         = string
+      from_port       = number
+      to_port         = number
       protocol        = string
       cidr_blocks     = list(string)
-      prefix_list_ids = list(string)
       security_groups = list(string)
+      prefix_list_ids = list(string)
       self            = bool
       description     = string
     }))
+
     egress = list(object({
-      from_port       = string
-      to_port         = string
+      from_port       = number
+      to_port         = number
       protocol        = string
       prefix_list_ids = list(string)
       cidr_blocks     = list(string)
       description     = string
     }))
   }))
-  description = <<EOF
-    - description: (string) Security group description. Defaults to Managed by Terraform. Cannot be "". NOTE: This field maps to the AWS GroupDescription attribute, for which there is no Update API. If you'd like to classify your security groups in a way that can be updated, use tags.
-    - vpc_id: (string) VPC ID. Defaults to the region's default VPC.
-    - service: (string) AWS Service (ecs - alb - rds)
-    - application: (string) Application name in order to name security group.
-    - functionality: (optional, string) Functionality name
-    - ingress:
-      - description: (string) Description of this ingress rule.
-      - from_port: (string) Start port (or ICMP type number if protocol is icmp or icmpv6).
-      - to_port: (string) End range port (or ICMP code if protocol is icmp).
-      - protocol: (string) Protocol. If you select a protocol of -1 (semantically equivalent to all, which is not a valid value here), you must specify a from_port and to_port equal to 0. The supported values are defined in the IpProtocol argument on the IpPermission API reference. This argument is normalized to a lowercase value to match the AWS API requirement when using with Terraform 0.12.x and above, please make sure that the value of the protocol is specified as lowercase when using with older version of Terraform to avoid an issue during upgrade.
-      - prefix_list_ids: (list(string)) List of Prefix List IDs.
-      - cidr_blocks: (list(string)) List of CIDR blocks.
-      - security_groups: (list(string)) List of security groups. A group name can be used relative to the default VPC. Otherwise, group ID.
-      - self: (bool) Whether the security group itself will be added as a source to this ingress rule.
-    - egress:
-      - description: (string) Description of this egress rule.
-      - from_port: (string) Start port (or ICMP type number if protocol is icmp)
-      - to_port: (string) End range port (or ICMP code if protocol is icmp).
-      - protocol: (string) Protocol. If you select a protocol of -1 (semantically equivalent to all, which is not a valid value here), you must specify a from_port and to_port equal to 0. The supported values are defined in the IpProtocol argument in the IpPermission API reference. This argument is normalized to a lowercase value to match the AWS API requirement when using Terraform 0.12.x and above. Please make sure that the value of the protocol is specified as lowercase when used with older version of Terraform to avoid issues during upgrade.
-      - prefix_list_ids: (list(string)) List of Prefix List IDs.
-      - cidr_blocks: (list(string)) List of CIDR blocks.
-  EOF
+  description = "Mapa de configuración de Security Groups"
 }
