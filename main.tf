40f0df1c476757a7415c695358fe1d87fc8041fc
farciniegas52 <felipe.arciniegas@pragma.com.co>
2025-03-11 17:31:33 -0500

    Update SG

--- main.tf
+++ main.tf
@@ -1,53 +1,67 @@
 ###########################################
 ####### Security Group Resources ##########
 ###########################################
-
 resource "aws_security_group" "sg" {
   provider    = aws.project
   for_each    = var.sg_config
 
-  name        = join("-", [var.client, var.project, var.environment, "sg", each.value.service, each.value.application, each.value.functionality])
+  name        = join("-", [var.client, var.project, var.environment, "sg", each.value.service, each.value.application])
   description = each.value.description
   vpc_id      = each.value.vpc_id
-
-  dynamic "ingress" {
-    for_each = each.value.ingress
-    content {
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-
-      security_groups = [
-        for sg in ingress.value.security_groups :
-        (
-          contains(keys(aws_security_group.sg), sg) ? aws_security_group.sg[sg].id : (
-            can(regex("^sg-", sg)) ? sg : (
-              "INVALID_SG_REFERENCE"
-            )
-          )
-        )
-      ]
-
-      prefix_list_ids = ingress.value.prefix_list_ids
-      self            = ingress.value.self
-      description     = ingress.value.description
-    }
-  }
-
-  dynamic "egress" {
-    for_each = each.value.egress
-    content {
-      from_port       = egress.value.from_port
-      to_port         = egress.value.to_port
-      protocol        = egress.value.protocol
-      cidr_blocks     = egress.value.cidr_blocks
-      prefix_list_ids = egress.value.prefix_list_ids
-      description     = egress.value.description
-    }
-  }
-
-  tags = {
+   tags = {
     Name = join("-", [var.client, var.project, var.environment, "sg", each.value.service, each.value.application])
   }
+}
+
+# Create Rule Ingress
+resource "aws_security_group_rule" "ingress_rules" {
+  provider    = aws.project
+  for_each = {
+    for idx, rule in flatten([
+      for sg_key, sg in var.sg_config : [
+        for i, ingress in sg.ingress : {
+          sg_key     = sg_key
+          rule       = ingress
+          idx        = "${sg_key}_ingress_${i}"
+        }
+      ]
+    ]) : rule.idx => rule
+  }
+
+  type              = "ingress"
+  from_port         = each.value.rule.from_port
+  to_port           = each.value.rule.to_port
+  protocol          = each.value.rule.protocol
+  security_group_id = aws_security_group.sg[each.value.sg_key].id
+  cidr_blocks       = each.value.rule.self == false && length(each.value.rule.cidr_blocks) > 0 ? each.value.rule.cidr_blocks : null
+  source_security_group_id = (contains(keys(each.value.rule), "security_groups") && each.value.rule.self == false && length(each.value.rule.security_groups) > 0) ? (startswith(each.value.rule.security_groups[0], "sg-") ? each.value.rule.security_groups[0] : aws_security_group.sg[each.value.rule.security_groups[0]].id) : null
+  self              = each.value.rule.self == true ? true : null
+  prefix_list_ids   = each.value.rule.self == false && length(each.value.rule.prefix_list_ids) > 0 ? each.value.rule.prefix_list_ids : null
+  description       = each.value.rule.description
+}
+
+# Create Rule Egress
+resource "aws_security_group_rule" "egress_rules" {
+  provider    = aws.project
+  for_each = {
+    for idx, rule in flatten([
+      for sg_key, sg in var.sg_config : [
+        for i, egress in sg.egress : {
+          sg_key     = sg_key
+          rule       = egress
+          idx        = "${sg_key}_egress_${i}"
+        }
+      ]
+    ]) : rule.idx => rule
+  }
+
+  type              = "egress"
+  from_port         = each.value.rule.from_port
+  to_port           = each.value.rule.to_port
+  protocol          = each.value.rule.protocol
+  security_group_id = aws_security_group.sg[each.value.sg_key].id
+  cidr_blocks       = each.value.rule.cidr_blocks
+  source_security_group_id = (contains(keys(each.value.rule), "security_groups") && length(each.value.rule.security_groups) > 0) ? (startswith(each.value.rule.security_groups[0], "sg-") ? each.value.rule.security_groups[0] : aws_security_group.sg[each.value.rule.security_groups[0]].id) : null
+  prefix_list_ids   = contains(keys(each.value.rule), "prefix_list_ids") ? each.value.rule.prefix_list_ids : null
+  description       = each.value.rule.description
 }
